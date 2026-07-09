61acb224b990dd2f418fcc14518ecd3b4cba83df
Gustavo Silva <gustavogeraldo14@gmail.com>
2026-07-08 21:53:06 -0300

    Adc session , instance, and database

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
+from models.user import User
+from database import db
 
 app = Flask(__name__)
-#Configuração da chave secreta para sessões e segurança
 app.config['SECRET_KEY'] = 'your_secret_key'
-#Caminho obrigatório para o banco de dados SQLite
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqllite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+db.init_app(app)
 
-
-db = SQLAlchemy(app)
 
 @app.route("/Hello-World", methods=['GET'])
 def hello_world():
